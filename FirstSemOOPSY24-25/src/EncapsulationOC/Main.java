4e4813b4f2ca57cbf9f0fcdef285bfd6a873c5fa
hp <hp@DESKTOP-VDURALK>
2024-09-23 21:04:01 +0800

    SetterGetter

---
+++ FirstSemOOPSY24-25/src/EncapsulationOC/Main.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EncapsulationOC;
+
+/**
+ *
+ * @author hp
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        /*In this scenario the first name that has been initialized in the SetterGetter class will become null
+        after the new instance of obj1 which is called data loss.
+        SetterGetter obj1 = new SetterGetter();
+        obj1.displayInfo();
+        obj1 = new SetterGetter("palma",19);
+        obj1.displayInfo();
+        */
+        
+        //C-create, R-retrive, U-update, D-delete
+        SetterGetter obj2 = new SetterGetter("Sonny","Palma",19);
+        obj2.displayInfo();
+        obj2.setFirstName("Hello");
+        obj2.displayInfo();
+        
+        //Delete
+        obj2.deleteAge();
+        obj2.setLastName("");
+        System.out.println("---------------------");
+        obj2.displayInfo();
+        
+    }
+}
---
+++ FirstSemOOPSY24-25/src/EncapsulationOC/SetterGetter.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EncapsulationOC;
+
+/**
+ *
+ * @author hp
+ */
+public class SetterGetter {
+    
+    private String firstName, lastName;
+    private int age;
+    
+    /*Overloading is creating same method but differs in parameter one is blank the other has 
+    public SetterGetter(){
+        this.firstName = "firstSam";
+    }
+    */
+    
+    public SetterGetter(String firstName, String lastName, int age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+    
+    //Retrieve
+    public String getFirstName(){
+        return firstName; 
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+    
+    public int getAge(){
+        return age;
+    }
+    
+    //Set or Update
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    
+    public void setAge(int age){
+        this.age = age;
+    }
+    
+    void displayInfo(){
+        System.out.println("First name: "+this.firstName+"\nLast name: "+getLastName()+"\nAge: "+this.age);
+        
+    }
+    
+    //Delete
+    public String deleteFirstName(){
+        return this.firstName = "";
+    }
+    
+    public String deleteLastName(){
+        return this.lastName = "";
+    }
+    
+    public int deleteAge(){
+        return this.age = 0;
+    }
+    
+    
+}
